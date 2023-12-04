c1d6cabee5423cd3cce9e7dd3d0a957a70d301bc
Marcin Pietrzyk <marcinpietrzyk7@gmail.com>
2023-12-04 14:56:26 +0100

    javascript

---
+++ Sprint3zadanie.js
@@ -0,0 +1,122 @@
+//Zadania ze zmiennych
+
+let x = 7;
+
+let firstName = "Marcin";
+
+let welcome = null;
+welcome = 'hello';
+
+const A = "Marcin 229";
+
+let a = true;
+const b = false;
+
+x=5;
+y=2;
+let result = x + y;
+
+const name = 'Marcin Pietrzyk';
+
+let number = 3.12364;
+
+let city = "Czestochowa";
+
+let  = "5+5";
+
+let result2 = x - y;
+
+//Zadania z prostych i złożonych typów danych
+
+arrayA = [1, 'Marcin', true];
+
+car = {
+    make: "Ferrari",
+    model: "F50",
+    year: 1995,
+};
+
+let arrayB = [7, 3.14, 25];
+
+personalData = {
+    name: "Marcin",
+    age: 39,
+    isStudent: true
+}
+
+let object = {};
+object.color = "red";
+
+Animals = [
+    cat = {
+        name: "Charles",
+        age: 3,
+    },
+    dog = {
+        name: "Thomas",
+        age: 7,
+    }
+]
+
+Marcin [isStudent=false, is39=true]
+
+Student = {
+    name: "Marcin",
+    grades: [5,5,4],
+    isActive: false,
+}
+
+ArrayC = [
+    country = {
+        name: Poland,
+        capital: Warsaw,
+    },
+    12, 33.1456, "Marcin", isStudent=false
+];
+
+Book = {
+    title: "Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    "year of publication": 1954,
+};
+
+//Zadania z warunków i operatorów trójargumentowych
+
+let number2 = 30;
+if (number > 10) {console.log("Liczba jest większa od 10")}
+else {console.log ("Liczba jest mniejsza lub równa 10")};
+
+let text2 = "Hello";
+if (text2 = "Hello") {console.log("Hello")};
+
+let summerTemperature = temperature >= 17 ? "ciepło" : "zimno";
+
+let age = 18;
+if (age >=18) {console.log("pełnoletni")}
+else {console.log("niepełnoletni")}
+
+let table1 = []
+if (table1 == []){console.log(true)}
+else {console.log("tablica nie jest pusta")};
+
+let isMember = member = true? "rabat" : "Rabat przysługuje tylko członkom";
+
+if (number3 >= 10000) {console.log(true)}
+else {console.log(false)}
+
+let xx = 7
+let yy = 2
+if (xx % yy == 1) {console.log(xx, 'to liczba parzysta')}
+else {console.log(xx,'to liczba nieparzysta')};
+
+let winterTemperature = temperature >= 0 ? "ciepło" : "zimno";
+
+let table2 = [1, 332244, x]
+switch (number = 332244) {
+  case table2[1] = 332244: console.log(true)
+  break;
+  case table2[2] = 332244: console.log(true)
+  break;
+  case table2[3] = 332244: console.log(true)
+    default: console.log(false)
+};
